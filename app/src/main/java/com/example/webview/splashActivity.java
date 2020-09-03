b6aae744a64b762196876e1a51e159f2a1ed14a6
kishorereddy23 <70091012+kishorereddy23@users.noreply.github.com>
2020-09-03 13:13:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webview/splashActivity.java
@@ -0,0 +1,23 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashActivity extends AppCompatActivity {
+private static int Splash_time=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },Splash_time);
+    }
+}
